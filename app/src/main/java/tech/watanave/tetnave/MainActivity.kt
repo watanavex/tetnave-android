485f645a12ccd375eb15024bc31d4f70e2734397
watanave <susan.se3p@gmail.com>
2020-12-29 09:54:54 +0900

    import jetpack compose

--- app/src/main/java/tech/watanave/tetnave/MainActivity.kt
+++ app/src/main/java/tech/watanave/tetnave/MainActivity.kt
@@ -2,10 +2,23 @@ package tech.watanave.tetnave
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContent {
+            ActivityContent()
+        }
+    }
+
+    @Preview
+    @Composable
+    private fun ActivityContent() {
+        Text("Android")
     }
 }
