afb6c0bf63684a8b9d9b413d1a886142ee626ff2
watanavex <y_watanabe@yumemi.co.jp>
2021-01-07 23:08:27 +0900

    Add Util class tests

---
+++ app/src/test/java/tech/watanave/tetnave/game/UtilTest.kt
@@ -0,0 +1,16 @@
+package tech.watanave.tetnave.game
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class UtilTest {
+
+    @Test
+    fun move() {
+        val position = Position(3, 6)
+            .move(2, -1)
+
+        assertThat(position).isEqualTo(Position(5, 5))
+    }
+
+}
